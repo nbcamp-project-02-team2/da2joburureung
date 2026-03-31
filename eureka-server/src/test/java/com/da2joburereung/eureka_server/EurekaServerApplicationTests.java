392a3f2ba08c48e2201433ebe0130ecbc9f12228
seongxun <rjqlrl30@naver.com>
2026-03-31 12:20:43 +0900

    feat: eureka server module 추가 및 초기 설정

---
+++ eureka-server/src/test/java/com/da2joburereung/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da2joburereung.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
