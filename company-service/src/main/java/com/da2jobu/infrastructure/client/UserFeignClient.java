a409dd8d5f98c0aac87576205c18b6b6ad308ed2
danbeekimm <eksql8061@naver.com>
2026-04-06 01:24:23 +0900

    import 문 삭제

--- company-service/src/main/java/com/da2jobu/infrastructure/client/UserFeignClient.java
+++ company-service/src/main/java/com/da2jobu/infrastructure/client/UserFeignClient.java
@@ -5,7 +5,6 @@ import common.dto.CommonResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.UUID;
 
