9e6727152c5dedbe469aca2bf9dd1c30b231a25a
seongxun <rjqlrl30@naver.com>
2026-04-07 00:35:04 +0900

    [feat] delivery-service OpenAPI 설정 및 API 문서화 추가 3차

--- delivery-service/src/main/java/com/da2jobu/deliveryservice/presentation/deliveryManager/dto/request/UpdateDeliveryManagerRequest.java
+++ delivery-service/src/main/java/com/da2jobu/deliveryservice/presentation/deliveryManager/dto/request/UpdateDeliveryManagerRequest.java
@@ -11,7 +11,7 @@ public record UpdateDeliveryManagerRequest(
         @Schema(description = "허브 ID", example = "22222222-2222-2222-2222-222222222222", nullable = true)
         UUID hubId,
 
-        @Schema(description = "배송 담당자 타입", example = "COMPANY_DELIVERY_MANAGER")
+        @Schema(description = "배송 담당자 타입", example = "COMPANY_DELIVERY")
         @NotNull(message = "배송 매니저 타입은 필수입니다.")
         DeliveryManagerType type
 ) {
