fbc639190d785abd123e4360bfec993f9b37e78a
seongxun <rjqlrl30@naver.com>
2026-04-07 02:36:07 +0900

    [feat] hub-service swagger 적용

--- hub-service/src/main/java/com/delivery/hub/application/dto/CreateHubCommand.java
+++ hub-service/src/main/java/com/delivery/hub/application/dto/CreateHubCommand.java
@@ -4,7 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "허브 생성 커맨드")
 public record CreateHubCommand(
+        @Schema(description = "허브 이름", example = "경기 남부 허브")
         String hub_name,
+
+        @Schema(description = "허브 주소", example = "경기도 이천시 부발읍 중부대로 123")
         String address
 ) {
     public static CreateHubCommand of(String hub_name, String address) {
--- hub-service/src/main/java/com/delivery/hub/application/dto/SearchHubCommand.java
+++ hub-service/src/main/java/com/delivery/hub/application/dto/SearchHubCommand.java
@@ -6,8 +6,14 @@ import java.util.UUID;
 
 @Schema(description = "허브 검색 커맨드")
 public record SearchHubCommand(
+
+        @Schema(description = "허브 ID", example = "11111111-1111-1111-1111-111111111111", nullable = true)
         UUID hub_id,
+
+        @Schema(description = "허브 이름 검색어", example = "경기", nullable = true)
         String hub_name,
+
+        @Schema(description = "주소 검색어", example = "이천", nullable = true)
         String address
 ){
     public static SearchHubCommand of(UUID hub_id,String hub_name, String address) {
